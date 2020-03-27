97d9088b408352d7cbe6fea1f024c4a248760842
Markus <46903097+mskg@users.noreply.github.com>
2020-03-27 22:36:12 +0100

    Update company.py

--- backend/api/src/schema/types/company.py
+++ backend/api/src/schema/types/company.py
@@ -8,9 +8,9 @@ class Company(ObjectType):
     id = ID(required=True)
     name = String(required=True)
     
-    street1 = String(required=False)
-    street2 = String(required=False)
-    street3 = String(required=False)
+    line1 = String(required=False)
+    line2 = String(required=False)
+    line3 = String(required=False)
 
     postal_code = String(required=True)
     city = String(required=True)
