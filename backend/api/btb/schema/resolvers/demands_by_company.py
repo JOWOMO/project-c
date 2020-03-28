e5952d36cfd5489fab73bbe87e44a8480be16949
Markus Kling <markus.kling@gmail.com>
2020-03-28 15:56:23 +0100

    Remove obsolete db package, enable poetry in serverless

--- backend/api/btb/schema/resolvers/demands_by_company.py
+++ backend/api/btb/schema/resolvers/demands_by_company.py
@@ -1,6 +1,4 @@
 from graphene import ID, String, ObjectType
-from db import Company as DB_Company
-
 
 def demands_by_company(root, info):
     context: Context = info.context
--- backend/api/btb/schema/types/user.py
+++ backend/api/btb/schema/types/user.py
@@ -2,7 +2,6 @@ from graphene import ID, String, ObjectType, List
 from .company import Company
 from btb.schema.resolvers import company_by_principal
 
-
 class User(ObjectType):
     id = ID(required=True)
     email = String(required=True)
