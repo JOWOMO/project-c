828160c1b63f716fec80c35cd21b0c9ac5d96fc9
Markus Kling <markus.kling@gmail.com>
2020-03-31 18:55:00 +0200

    Frontend deployment workflow

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+
+if (!fs.existsSync('aws.json')) {
+  console.error(`please run: npm run config:aws`);
+  throw `No config file`;
+}
+
+// reads the dependencies from the cloudformation stack
+const awsConfig = JSON.parse(fs.readFileSync('aws.json'));
+function findAWSExport(setting) {
+  const node = awsConfig.find((n) => n.ExportName === `${setting}-${process.env.STAGE || 'dev'}`);
+  if (!node) throw `Setting ${setting} is not known`;
+
+  console.log(setting, ':', node.OutputValue)
+  return node.OutputValue;
+}
 
 export default {
   mode: 'universal',
@@ -55,12 +71,20 @@ export default {
     // Doc: https://github.com/nuxt-community/apollo-module
     '@nuxtjs/apollo'
   ],
+
+  env: {
+    region: 'eu-west-1',
+    userPoolId: findAWSExport('CognitoUserPool'),
+    identityPoolId: findAWSExport('CognitoIdentityPool'),
+    userPoolWebClientId: findAWSExport('CognitoUserPoolClient'),
+  },
+
   /*
   ** Apollo module configuration
   ** See https://github.com/nuxt-community/apollo-module
   */
   apollo: {
-    tokenName: 'yourApolloTokenName', // optional, default: apollo-token
+    tokenName: 'apollo-token', // optional, default: apollo-token
     cookieAttributes: {
       /*
       ** Define when the cookie will be removed. Value can be a Number
@@ -72,12 +96,12 @@ export default {
       /**
         * Define the path where the cookie is available. Defaults to '/'
         */
-      path: '/', // optional
+      // path: '/', // optional
       /**
         * Define the domain where the cookie is available. Defaults to
         * the domain of the page where the cookie was created.
         */
-      domain: 'example.com', // optional
+      // domain: 'example.com', // optional
 
       /**
         * A Boolean indicating if the cookie transmission requires a
@@ -86,7 +110,7 @@ export default {
       secure: false,
     },
     includeNodeModules: true, // optional, default: false (this includes graphql-tag for node_modules folder)
-    authenticationType: 'Basic', // optional, default: 'Bearer'
+    authenticationType: '', // required to be empty
     // (Optional) Default 'apollo' definition
     defaultOptions: {
       // See 'apollo' definition
@@ -104,34 +128,33 @@ export default {
     clientConfigs: {
       default: {
         // required
-        httpEndpoint: 'http://localhost:4000',
+        httpEndpoint: `https://${findAWSExport('ApiGatewayRestApiId')}.execute-api.eu-west-1.amazonaws.com/dev/graphql`,
+
         // optional
         // override HTTP endpoint in browser only
-        browserHttpEndpoint: '/graphql',
+        // browserHttpEndpoint: '/graphql',
+
         // optional
         // See https://www.apollographql.com/docs/link/links/http.html#options
         httpLinkOptions: {
           credentials: 'same-origin'
         },
+
         // You can use `wss` for secure connection (recommended in production)
         // Use `null` to disable subscriptions
-        wsEndpoint: 'ws://localhost:4000', // optional
+        // wsEndpoint: 'ws://localhost:4000', // optional
+
         // LocalStorage token
         tokenName: 'apollo-token', // optional
+
         // Enable Automatic Query persisting with Apollo Engine
         persisting: false, // Optional
+
         // Use websockets for everything (no HTTP)
         // You need to pass a `wsEndpoint` for this to work
-        websocketsOnly: false // Optional
+        // websocketsOnly: false // Optional
       },
-      test: {
-        httpEndpoint: 'http://localhost:5000',
-        wsEndpoint: 'ws://localhost:5000',
-        tokenName: 'apollo-token'
-      },
-      // alternative: user path to config which returns exact same config options
-      test2: '~/plugins/my-alternative-apollo-config.js'
-    }
+    },
   },
   /*
   ** Axios module configuration
