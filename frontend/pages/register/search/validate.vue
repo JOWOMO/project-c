fcb709c1593161de61b5931267be27bb035f6f93
HannHank <hanke.johann@gmail.com>
2020-04-01 10:36:46 +0200

    email parsing in validate

--- frontend/pages/register/search/validate.vue
+++ frontend/pages/register/search/validate.vue
@@ -79,7 +79,7 @@ export default {
   data() {
     return {
       user: {
-        email: "",
+        email: this.$store.state.register_state.user.email,
         code: null
       },
       submitted: false,
