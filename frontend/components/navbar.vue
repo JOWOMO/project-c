cd1a60e7298c1e30840f55b00bf0e76384502244
johann-su <48410810+johann-su@users.noreply.github.com>
2020-04-07 15:45:54 +0200

    Feature/navbar (#25)
    * added navigation buttons
    
    * added navbar links
    
    Co-authored-by: “[Johann <“[johann123.js@gmail.com]”>

--- frontend/components/navbar.vue
+++ frontend/components/navbar.vue
@@ -4,31 +4,37 @@
       <img src="/images/logo.svg" alt="Logo" class="logo" />
     </nuxt-link>
 
-    <div v-if="!$store.state.auth.isAuthenticated" class="links">
-      <nuxt-link to="/info" class="link">FAQ</nuxt-link>
-      <button class="primary" @click="$router.push('/login')">Login</button>
-    </div>
-
-    <div v-else class="profile" @click="active = !active">
-      <div class="nameImg">
-        <span>{{ title }}</span>
-        <!-- TODO: Add img from database -->
-        <img src="/images/profile.jpg" alt class="profile_img" />
+    <div class="right">
+      <div class="links">
+        <nuxt-link to="/dashboard" class="link" :class="{active: search}">Meine Suchergebnisse</nuxt-link>
+        <nuxt-link to="/info" class="link" :class="{active: faq}">FAQ</nuxt-link>
       </div>
-      <div class="dropdown" :class="{expanded: active}">
-        <div class="options">
-          <button @click="new_password">Passwort ändern</button>
-          <button class="red" @click="warning = !warning">Benutzer löschen</button>
+
+      <div v-if="!$store.state.auth.isAuthenticated" class="links">
+        <button class="primary" @click="$router.push('/login')">Login</button>
+      </div>
+
+      <div v-else class="profile" @click="active = !active">
+        <div class="nameImg">
+          <span>{{ title }}</span>
+          <!-- TODO: Add img from database -->
+          <img src="/images/profile.jpg" alt class="profile_img" />
         </div>
-        <button class="blue" @click="logout">Logout</button>
+        <div class="dropdown" :class="{expanded: active}">
+          <div class="options">
+            <button @click="new_password">Passwort ändern</button>
+            <button class="red" @click="warning = !warning">Benutzer löschen</button>
+          </div>
+          <button class="blue" @click="logout">Logout</button>
+        </div>
       </div>
-    </div>
 
-    <div class="warning" :class="{expanded: warning}">
-      <h2>Möchtest du deinen Account Löschen?</h2>
-      <p>Alle Daten werden gelöscht</p>
-      <button @click="warning = !warning">Abbrechen</button>
-      <button class="red" @click="deleteUser">Löschen</button>
+      <div class="warning" :class="{expanded: warning}">
+        <h2>Möchtest du deinen Account Löschen?</h2>
+        <p>Alle Daten werden gelöscht</p>
+        <button @click="warning = !warning">Abbrechen</button>
+        <button class="red" @click="deleteUser">Löschen</button>
+      </div>
     </div>
   </nav>
 </template>
@@ -48,7 +54,9 @@ export default {
       login: false,
       title: "",
       active: false,
-      warning: false
+      warning: false,
+      search: true,
+      faq: false
     };
   },
 
@@ -88,6 +96,19 @@ export default {
     }
   },
 
+  created() {
+    if(this.$route.path == '/dashboard') {
+      this.search = true
+      this.faq = false
+    } else if (this.$route.path == '/info') {
+      this.faq = true
+      this.search = false
+    } else {
+      this.search = false
+      this.faq = false
+    }
+  },
+
   computed: {
     isAuthenticated() {
       // console.log(this.$store.state.auth.isAuthenticated);
@@ -98,6 +119,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '@/assets/colors';
+
 nav {
   width: 100vw;
   height: 120px;
@@ -111,6 +134,22 @@ nav {
 
   .profile, .links {
     display: inline-block;
+  }
+
+  .links {
+    margin-right: 20px;
+
+    .link {
+      display: inline-block;
+      color: $fontColor;
+      margin: 5px;
+      font-size: 18px;
+      height: 40px;
+
+      &.active {
+        border-bottom: 4px solid $primary;
+      }
+    }
   }
 
   .profile {
@@ -160,15 +199,15 @@ nav {
     }
   }
 
-  .links {
-    .link {
-      margin-right: 88px;
-    }
+  // .links {
+  //   .link {
+  //     margin-right: 88px;
+  //   }
 
-    a {
-          font-size: 18px !important;
-    }
-  }
+  //   a {
+  //         font-size: 18px !important;
+  //   }
+  // }
 
   .warning {
     position: absolute;
