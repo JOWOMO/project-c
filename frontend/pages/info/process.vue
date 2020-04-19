4003c006f9fe0a19381e9bfe4117484264dc2c7d
Markus Kling <markus.kling@gmail.com>
2020-04-19 16:06:27 +0200

    Texts, fixes #105

--- frontend/pages/info/process.vue
+++ frontend/pages/info/process.vue
@@ -15,28 +15,27 @@
         </template>
       </pe>
 
-      <pe number="1" title="Anmelden">
+      <pe number="1" title="Melde Dich an">
         <template #both>Mit ein paar Clicks bei JOWOMO anmelden und E-Mail-Adresse bestätigen.</template>
       </pe>
 
-      <pe number="2" title="Profil erstellen">
+      <pe number="2" title="Erstelle Dein Profil">
         <template #both>Registrier Dein Unternehmen bei JOWOMO.</template>
       </pe>
 
-      <pe number="3" title="Team(s) erstellen">
-        <template #left>Lass uns wissen, welche Arbeitskräfte Du suchst.</template>
-        <template #right>Lass uns wissen, welche Arbeitskräfte gerade bei Dir verfügbar sind.</template>
+      <pe number="3" title="Stelle Team(s) ein">
+        <template #left>Lass andere Unternehmen wissen, welche Arbeitskräfte du suchst.</template>
+        <template #right>Lass  andere Unternehmen wissen, welche Arbeitskräfte gerade bei dir verfügbar sind.</template>
       </pe>
 
-      <pe number="4" title="Suchen">
+      <pe number="4" title="Starte Deine Suche">
         <template #both>Über die Suchfunktion findest Du Angebote in Deiner Umgebung.</template>
       </pe>
 
-      <pe number="5" title="Anfrage">
-        <template
-          #left
-        >Eine Firma möchte Dein Team einstellen. Hier hast Du Gelegenheit Details zu besprechen.</template>
-        <template #right>Lass uns wissen, welche Arbeitskräfte gerade verfügbar sind.</template>
+      <pe number="5" title="Wir verbinden Euch">
+        <template #both>
+          Du einen passenden Personalpartner entdeckt? Sende per Klick direkt eine Anfrage, damit ihr Details besprechen könnt.
+        </template>
       </pe>
 
       <pe number="6" title="Match">
@@ -48,13 +47,13 @@
         >Glückwunsch! Ihr entscheidt Euch für die Partnerschaft und leitet die nächsten Schritte ein.</template>
       </pe>
 
-      <pe number="7" title="Vertragliche Regelung">
+      <pe number="7" title="Triff eine vertragliche Regelung">
         <template
           #both
-        >Glückwunsch! Ihr entscheidt Euch für die Partnerschaft und leitet die nächsten Schritte ein.</template>
+        >Es gibt verschieden Möglichkeiten zur Umsetzung, die du für deinen Fall rechtlich prüfen lassen solltest. <nuxt-link to="/info/faq">Siehe auch unsere FAQs</nuxt-link></template>
       </pe>
 
-      <pe number="8" title="Offboarding" titleRight="Einarbeitung">
+      <pe number="8" title="Boarde deine Mitarbeiter:innen off " titleRight="Boarde deine Mitarbeiter:innen on">
         <template #left>Hilf Deinen Arbeitskräften in der Übergangsphase.</template>
         <template #right>Hilf Deinen neuen Arbeitskräften sich in Deinem Betrieb wohl zu fühlen.</template>
       </pe>
