8bd79068f63a699ba6bf821b0a070cd5618e2452
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2020-04-23 08:49:55 +0200

    Beta logo (#162)
    * add beta logo, resolve #125

--- frontend/components/logo.vue
+++ frontend/components/logo.vue
@@ -1,5 +1,19 @@
 <template>
   <nuxt-link to="/">
-    <img width="234px" height="37px" src="/images/logo.svg" />
+    <img v-if="useBetaLogo" width="234px" height="37px" src="/images/logo-beta.svg" />
+    <img v-else width="234px" height="37px" src="/images/logo.svg" />
   </nuxt-link>
 </template>
+
+<script lang="ts">
+console.log("hello")
+import { Vue, Component }  from "nuxt-property-decorator";
+
+@Component
+export default class extends Vue {
+  get useBetaLogo(): boolean {
+    // @ts-ignore
+    return process.env.useBetaLogo == 'true' || process.env.useBetaLogo === true;
+  }
+}
+</script>
