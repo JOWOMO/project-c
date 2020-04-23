79cf33807021ea81ed8bb3a28f83400cd283d5d1
Markus Kling <markus.kling@gmail.com>
2020-04-23 10:09:02 +0200

    Make the logo appearence less flicker

--- frontend/components/logo.vue
+++ frontend/components/logo.vue
@@ -1,19 +1,30 @@
-<template>
-  <nuxt-link to="/">
-    <img v-if="useBetaLogo" width="234px" height="37px" src="/images/logo-beta.svg" />
-    <img v-else width="234px" height="37px" src="/images/logo.svg" />
-  </nuxt-link>
-</template>
-
 <script lang="ts">
-console.log("hello")
-import { Vue, Component }  from "nuxt-property-decorator";
+import { CreateElement } from "vue";
+import { Vue, Component } from "nuxt-property-decorator";
+const LOGO = process.env.useBetaLogo
+    ? "/images/logo-beta.svg"
+    : "/images/logo.svg";
 
 @Component
 export default class extends Vue {
-  get useBetaLogo(): boolean {
-    // @ts-ignore
-    return process.env.useBetaLogo == 'true' || process.env.useBetaLogo === true;
+  render(h: CreateElement) {
+    return h(
+      "nuxt-link",
+      {
+        attrs: {
+          to: "/"
+        }
+      },
+      [
+        h("img", {
+          attrs: {
+            width: "234px",
+            height: "37px",
+            src: LOGO
+          }
+        })
+      ]
+    );
   }
 }
 </script>
